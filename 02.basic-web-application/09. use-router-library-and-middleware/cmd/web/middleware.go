e11fc373cdd2337a3f65739a0c9aadede835a776
tomonakar <tomohisa.nak@gmail.com>
2021-12-07 20:57:12 +0900

    feat(09): セッションを導入

--- 02.basic-web-application/09. use-router-library-and-middleware/cmd/web/middleware.go
+++ 02.basic-web-application/09. use-router-library-and-middleware/cmd/web/middleware.go
@@ -22,7 +22,7 @@ func NoSurf(next http.Handler) http.Handler {
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
