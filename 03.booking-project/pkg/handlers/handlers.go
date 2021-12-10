8cf616402e07382a2f8c992584a21631aacbeadb
tomonakar <tomohisa.nak@gmail.com>
2021-12-10 12:31:16 +0900

    feat(03): post処理を追加

--- 03.booking-project/pkg/handlers/handlers.go
+++ 03.booking-project/pkg/handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/tomonar/booking/pkg/config"
@@ -78,6 +80,29 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	end := r.Form.Get("end")
 
 	w.Write([]byte(fmt.Sprintf("start data is %s and end data is #s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and send JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available!",
+	}
+
+	// MarshalIndentは、JSONを書き込む時にインデントとprefixを付与する
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	// JSONをHTTPレスポンスで送る際には、ヘッダーにContent-Typeを設定する
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
