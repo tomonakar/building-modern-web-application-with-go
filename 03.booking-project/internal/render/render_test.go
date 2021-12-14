1bac1513649b335560c2da5a0b5451271fa84e82
tomonakar <tomohisa.nak@gmail.com>
2021-12-14 18:30:15 +0900

    test: renderのテストを追加

--- 03.booking-project/internal/render/render_test.go
+++ 03.booking-project/internal/render/render_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestAddDefaultData(t *testing.T) {
 	var td models.TemplateData
-	r, err := getSession(t)
+	r, err := getSession()
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,7 +22,30 @@ func TestAddDefaultData(t *testing.T) {
 	}
 }
 
-func getSession(t *testing.T) (*http.Request, error) {
+func TestRenderTemplate(t *testing.T) {
+	pathToTemplates = "./../../templates"
+	tc, err := CreateTemplateCache()
+	if err != nil {
+		t.Error(err)
+	}
+	app.TemplateCache = tc
+
+	r, err := getSession()
+
+	var ww myWriter
+
+	err = RenderTemplate(&ww, r, "home.page.tmpl", &models.TemplateData{})
+	if err != nil {
+		t.Error("error writing template to browser")
+	}
+
+	err = RenderTemplate(&ww, r, "non-existent.page.tmpl", &models.TemplateData{})
+	if err == nil {
+		t.Error("rendered template that does not exist")
+	}
+}
+
+func getSession() (*http.Request, error) {
 
 	r, err := http.NewRequest("GET", "/some-url", nil)
 	if err != nil {
@@ -33,4 +56,16 @@ func getSession(t *testing.T) (*http.Request, error) {
 	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
 	r = r.WithContext(ctx)
 	return r, nil
+}
+
+func TestNewTemplates(t *testing.T) {
+	NewTemplates(app)
+}
+
+func TestCreateTemplateCache(t *testing.T) {
+	pathToTemplates = "./../../templates"
+	_, err := CreateTemplateCache()
+	if err != nil {
+		t.Error(err)
+	}
 }
