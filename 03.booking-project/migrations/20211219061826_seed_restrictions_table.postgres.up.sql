3e0ad2501fa96ce64fe9a949ebacc6a9791eb67c
tomonakar <tomohisa.nak@gmail.com>
2021-12-19 15:33:16 +0900

    chore: seeder追加

---
+++ 03.booking-project/migrations/20211219061826_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,13 @@
+insert into
+  public.restrictions (restriction_name, created_at, updated_at)
+values
+  (
+    'Room1',
+    '2020-11-18 00:00:00.000',
+    '2020-11-18 00:00:00.000'
+  ),
+  (
+    'Room2',
+    '2020-12-19 00:00:00.000',
+    '2020-12-19 00:00:00.000'
+  );
