9f12a470758cecac7d68c122cfa2176bb577c525
tomonakar <tomohisa.nak@gmail.com>
2021-12-21 18:13:05 +0900

    feat: ログイン制御を行うミドルウェアを作成

--- 03.booking-project/cmd/web/middleware.go
+++ 03.booking-project/cmd/web/middleware.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/justinas/nosurf"
+	"github.com/tomonar/booking/internal/helpers"
 )
 
 func WriteToConsole(next http.Handler) http.Handler {
@@ -31,4 +32,16 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and saves session data for current request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			app.Session.Put(r.Context(), "error", "You must be logged in first")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+
 }
